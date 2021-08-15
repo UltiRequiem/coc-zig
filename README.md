05cab93224c6bff67f4d91e5d8dbd42771d1b7d2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-15 14:23:37 -0500

    :tada: Init Project

---
+++ README.md
@@ -0,0 +1,23 @@
+# coc-chuy
+
+
+
+## Install
+
+`:CocInstall coc-chuy`
+
+## Keymaps
+
+`nmap <silent> <C-l> <Plug>(coc-coc-chuy-keymap)`
+
+## Lists
+
+`:CocList demo_list`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { ExtensionContext, services, workspace, LanguageClient } from 'coc.nvim';
+
+async function activate(context: ExtensionContext): Promise<void> {
+  const config = workspace.getConfiguration('coc-zig');
+
+  if (!config.get<boolean>('enable', true)) return;
+
+  const serverOptions = {
+    command: 'zls',
+  };
+
+  const clientOptions = {
+    documentSelector: ['zig'],
+  };
+
+  const client = new LanguageClient('coc-zls', 'coc-zls', serverOptions, clientOptions);
+  context.subscriptions.push(services.registLanguageClient(client));
+}
+
+export default activate;
