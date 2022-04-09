3d4dc52f07c52a9e5e6facc639b691adac13e964
Mark Orel <mail.ormark@gmail.com>
2022-04-09 19:36:06 +0100

    fix: Replaces default export with named
    Scope: in order for this plugin to be activated named export should be
    used in the resulting bundle. `exports.activate` according to the
    documentation.

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import {
 } from "coc.nvim";
 import { LSP_NAME } from "./constants";
 
-export default async function activate(context: ExtensionContext): Promise<void> {
+export async function activate(context: ExtensionContext): Promise<void> {
   const config = workspace.getConfiguration("zig");
 
   const zlsPath = config.get("path", "");
